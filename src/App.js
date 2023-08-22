fd6735c3197677e1ebf637e0ebe9c5ec96d1a0d2
Asifa-khatoon <asifa441.xwave@gmail.com>
2023-08-21 21:33:58 -0700

    first project react js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,32 @@
 
 import './App.css';
 import Header from './components/Header';
+import Navmid from './components/Midnav';
+import SecLast from './components/Second-last';
+import TextSection from './components/Text-sec';
+import Section from './components/Third-section';
+import TheCards from './components/Three-cards';
+import GridSec from './components/Two-sections';
+import Footer from './components/Footerr.';
+
+
 
 
 function App() {
   return (
+    <> 
    <Header /> 
+  
+   <Navmid />
+   <Section />
+   <GridSec />
+   <TextSection />
+   <TheCards />
+   <SecLast />
+  <Footer />
+
+   </>    
+  
   );
 }
 
